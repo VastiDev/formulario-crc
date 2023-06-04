08bc939bba88f9bbecfbcdc20c032b58ba2a2089
vastidev <Vastiane1@gmail.com>
2023-06-04 08:48:38 -0300

    inicia o projeto

---
+++ src/main/java/com/wakandaacademy/formulariocrc/FormularioCrcApplication.java
@@ -0,0 +1,13 @@
+package com.wakandaacademy.formulariocrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormularioCrcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormularioCrcApplication.class, args);
+	}
+
+}
