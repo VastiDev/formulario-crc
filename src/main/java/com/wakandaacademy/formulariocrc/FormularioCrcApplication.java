94c2921add2be8f6d3149b90d160578b6b05c87d
vastidev <Vastiane1@gmail.com>
2023-06-05 06:54:43 -0300

    testa api

--- src/main/java/com/wakandaacademy/formulariocrc/FormularioCrcApplication.java
+++ src/main/java/com/wakandaacademy/formulariocrc/FormularioCrcApplication.java
@@ -1,13 +1,25 @@
 package com.wakandaacademy.formulariocrc;
 
+import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@Data
+@RequestMapping("/")
 public class FormularioCrcApplication {
+	@GetMapping
+	public String getHomeTeste(){
+		return "Crc Odonto - API Home";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FormularioCrcApplication.class, args);
+
 	}
 
 }
