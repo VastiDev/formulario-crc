488cc870789a8a2039267794c48afceb169aee95
vastidev <Vastiane1@gmail.com>
2023-06-14 16:17:51 -0300

    finaliza fluxo getTodos

--- src/main/java/com/wakandaacademy/formulariocrc/cliente/application/api/ClienteListResponse.java
+++ src/main/java/com/wakandaacademy/formulariocrc/cliente/application/api/ClienteListResponse.java
@@ -2,6 +2,7 @@ package com.wakandaacademy.formulariocrc.cliente.application.api;
 
 import com.wakandaacademy.formulariocrc.cliente.domain.AreaInteresse;
 import com.wakandaacademy.formulariocrc.cliente.domain.Cliente;
+import lombok.Value;
 import org.hibernate.validator.constraints.br.CPF;
 
 import javax.validation.constraints.Email;
@@ -9,7 +10,9 @@ import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+@Value
 public class ClienteListResponse {
     private UUID idCliente;
     private String nomeCompleto;
@@ -19,6 +22,17 @@ public class ClienteListResponse {
     private String cpf;
 
     public static List<ClienteListResponse> converte(List<Cliente> clientes) {
-        return null;
+        return clientes.stream()
+                .map(ClienteListResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    private ClienteListResponse(Cliente cliente) {
+        this.idCliente = cliente.getIdCliente();
+        this.nomeCompleto = cliente.getNomeCompleto();
+        this.celular = cliente.getCelular();
+        this.areaInteresse = cliente.getAreaInteresse();
+        this.dataNascimento = cliente.getDataNascimento();
+        this.cpf = cliente.getCpf();
     }
 }
